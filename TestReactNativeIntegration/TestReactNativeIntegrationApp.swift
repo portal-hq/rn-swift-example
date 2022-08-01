d60a0e08717d142afbc7903166de01b1342e5341
Elijah Windsor <ewindsor@gmail.com>
2022-08-01 10:18:40 -1000

    first commit

---
+++ TestReactNativeIntegration/TestReactNativeIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestReactNativeIntegrationApp.swift
+//  TestReactNativeIntegration
+//
+//  Created by Elijah Windsor on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TestReactNativeIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
