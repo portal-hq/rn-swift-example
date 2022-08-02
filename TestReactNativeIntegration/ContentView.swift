4793f2922a17db51ec34d65e831de746094c2980
Elijah Windsor <ewindsor@gmail.com>
2022-08-01 15:58:34 -1000

    chore: updating ContentView.swift

--- TestReactNativeIntegration/ContentView.swift
+++ TestReactNativeIntegration/ContentView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+import UIKit
+import React
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct RctView: UIViewRepresentable {
+    func makeUIView(context: Context) -> RCTRootView {
+        RCTRootView(bundleURL: URL(string: "http://localhost:8081/index.bundle?platform=ios")!, moduleName: "HelloTest", initialProperties: nil, launchOptions: nil)
+    }
+    
+    func updateUIView(_ view: RCTRootView, context: Context) {
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    var body: some View {
+        Text("Hey there, I am from Swift")
+        RctView()
     }
+    
 }
